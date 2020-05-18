f201b19290fd8f896037325521d94982fb6b875d
spezzers <54096233+spezzers@users.noreply.github.com>
2020-05-18 18:12:25 +0100

    1.7 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -19,6 +19,10 @@ const App = () => {
     setNeutral(neutral + 1)
   }
 
+  const all = good + bad + neutral
+  const average = ((1 * good) - (1 * bad))/all || 0
+  const positive = `${good / all || 0}%`
+
   return (
     <div>
       <h1>Give feedback</h1>
@@ -26,9 +30,12 @@ const App = () => {
       <button onClick={handleNeutral()}>Neutral</button>
       <button onClick={handleBad()}>Bad</button>
       <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}</p>
     </div>
   )
 }
