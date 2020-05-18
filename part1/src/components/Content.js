19699a4dbf45336caf66b43936fbd32ea8fd0f28
spezzers <54096233+spezzers@users.noreply.github.com>
2020-05-18 10:20:32 +0100

    1.3 done

--- part1/src/components/Content.js
+++ part1/src/components/Content.js
@@ -4,9 +4,9 @@ import Part from './Part'
 const Content = (props) => {
     return (
         <>
-            <Part part={props.part1} exercises={props.exercises1}/>
-            <Part part={props.part2} exercises={props.exercises2}/>
-            <Part part={props.part3} exercises={props.exercises3}/>
+            <Part part={props.part1}/>
+            <Part part={props.part2}/>
+            <Part part={props.part3}/>
         </>
     )
 }
