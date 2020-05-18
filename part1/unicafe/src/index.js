9fbf943242f22c4e9ff97898019e12ef7dc912be
spezzers <54096233+spezzers@users.noreply.github.com>
2020-05-18 18:32:51 +0100

    1.8 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,39 +3,51 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 
+const Statistics = ({feedback}) => {
+
+  const all = feedback.good + feedback.bad + feedback.neutral
+  const average = ((1 * feedback.good) - (1 * feedback.bad))/all || 0
+  const positive = `${feedback.good / all || 0}%`
+
+
+  return(
+    <>
+      <h2>Statistics</h2>
+      <p>Good: {feedback.good}</p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}</p>
+    </>
+  )
+}
+
+
+
+
+
+
 const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
 
-  const handleGood = () => () => {
-    setGood(good + 1)
-  }
-  const handleBad = () => () => {
-    setBad(bad + 1)
-  }
-  const handleNeutral = () => () => {
-    setNeutral(neutral + 1)
+  const [ feedback, setFeedback ] = useState({
+    good: 0,
+    bad: 0,
+    neutral: 0
+  })
+
+  const handleFeedback = (type) => {
+    setFeedback( {...feedback, [type]: feedback[type] + 1})
   }
 
-  const all = good + bad + neutral
-  const average = ((1 * good) - (1 * bad))/all || 0
-  const positive = `${good / all || 0}%`
 
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={handleGood()}>Good</button>
-      <button onClick={handleNeutral()}>Neutral</button>
-      <button onClick={handleBad()}>Bad</button>
-      <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {all}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive}</p>
+      <button onClick={() => handleFeedback("good")}>Good</button>
+      <button onClick={() => handleFeedback("neutral")}>Neutral</button>
+      <button onClick={() => handleFeedback("bad")}>Bad</button>
+      <Statistics feedback={feedback}/>
     </div>
   )
 }
