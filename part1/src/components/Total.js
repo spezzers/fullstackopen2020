966a6a130a187811c19a4b82dd2067808d816ecd
spezzers <54096233+spezzers@users.noreply.github.com>
2020-05-18 10:49:51 +0100

    exercise corrections: Total component

--- part1/src/components/Total.js
+++ part1/src/components/Total.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Total = (props) => {
+    let total = 0
+    const sumTotal = props.parts.forEach(part => {
+        total += part.exercises
+    });
     return (
         <>
-            <p>Number of exercises {props.exercises}</p>
+            <p>Number of exercises {total}</p>
         </>
     )
 }
