8f8c8f704bd05b030ab8695a5587b7d85431bd8e
spezzers <54096233+spezzers@users.noreply.github.com>
2020-05-22 12:56:10 +0100

    2.9 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 
 
-const Persons = ({peeps}) => {
-  const listPeeps = peeps.map(
-    person => <p key={person.name}>{`${person.name} ${person.number}`}</p>
+const Persons = ({peeps, filter}) => {
+  const filteredPeeps = peeps.filter(person => person.name.toUpperCase().includes(filter.toUpperCase()))
+  const listPeeps = filteredPeeps.map(
+    person => <div key={person.name}>{`${person.name} ${person.number}`}</div>
   )
   return <>{listPeeps}</>
 }
@@ -12,17 +13,21 @@ const Persons = ({peeps}) => {
 const App = () => {
 
   const [ persons, setPersons ] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '0715369422'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
 
 
   const handleNameInput = event => setNewName(event.target.value)
   const handleNumberInput = event => setNewNumber(event.target.value)
+  const handleFilterInput = event => setFilter(event.target.value)
+
+  
 
   const addPerson = event => {
     event.preventDefault()
@@ -39,8 +44,11 @@ const App = () => {
 
   return (
     <div>
-      <div>debug: <br/>{newName} {newNumber}</div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter: <input value={filter} onChange={handleFilterInput}/>
+      </div>
+      <h2>Add new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameInput}/>
@@ -53,7 +61,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons peeps={persons}/>
+      <Persons peeps={persons} filter={filter}/>
     </div>
   )
 }
