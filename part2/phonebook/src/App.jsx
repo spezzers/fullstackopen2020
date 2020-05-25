fc1d26514ab92dea9d0e78b01a4d00e5c901b729
spezzers <54096233+spezzers@users.noreply.github.com>
2020-05-25 19:13:04 +0100

    2.18 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,62 +5,73 @@ import Persons from './components/Persons.jsx'
 import contactService from './services/contacts.js'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
+	const [persons, setPersons] = useState([])
+	const [newName, setNewName] = useState('')
+	const [newNumber, setNewNumber] = useState('')
+	const [filter, setFilter] = useState('')
 
-  useEffect(() => {
-    contactService.getAll().then(initial => {
-      setPersons(initial)
-    })
-  }, [])
+	useEffect(() => {
+		contactService.getAll().then(initial => {
+			setPersons(initial)
+		})
+	}, [])
 
-  const handleNameInput = event => setNewName(event.target.value)
-  const handleNumberInput = event => setNewNumber(event.target.value)
-  const handleFilterInput = event => setFilter(event.target.value)
+	const handleNameInput = event => setNewName(event.target.value)
+	const handleNumberInput = event => setNewNumber(event.target.value)
+	const handleFilterInput = event => setFilter(event.target.value)
+	const clearForm = () => {
+		setNewName('')
+		setNewNumber('')
+	}
 
-  const addPerson = () => {
-    if (persons.map(person => person.name).includes(newName)) {
-      alert(`'${newName}' is already in the phonebook`)
-    } else {
-      contactService
-        .add({ name: newName, number: newNumber })
-        .then(res => {
-          setPersons(persons.concat(res))
-          setNewName('')
-          setNewNumber('')
-        })
-    }
-  }
-  const removePerson = (event) => {
-    const id = parseInt(event.target.id)
-    const deleteMe = persons.filter(p => p.id === id)[0]
-    
-    if (window.confirm(`Do you really want to delete ${deleteMe.name}`)) {
-      contactService
-        .remove(id)
-        .then(setPersons(
-          persons.filter(p => p.id !== id)
-        ))
-    }
-      
-  }
+	const addPerson = event => {
+		event.preventDefault()
+		const duplicate = persons.filter(
+			person => person.name.toUpperCase() === newName.toUpperCase()
+		)
+		const chosenOne = duplicate[0]
 
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      <Filter value={filter} onChange={handleFilterInput} />
-      <Form
-        nameValue={newName}
-        nameInput={handleNameInput}
-        numberValue={newNumber}
-        numberInput={handleNumberInput}
-        onSubmit={addPerson}
-      />
-      <Persons peeps={persons} filter={filter} remove={removePerson}/>
-    </div>
-  )
+		if (duplicate.length > 0) {
+			if (window.confirm(`'${newName}' is already in the phonebook, update number?`)) {
+				const updatedContact = { ...chosenOne, number: newNumber }
+				contactService.update(chosenOne.id, updatedContact).then(res => {
+          setPersons(persons.map(p => (p.id !== chosenOne.id ? p : res)))
+          clearForm()
+				})
+			}
+		} else {
+			contactService.add({ name: newName, number: newNumber }).then(res => {
+				setPersons(persons.concat(res))
+				clearForm()
+			})
+		}
+	}
+
+	const removePerson = event => {
+		const id = parseInt(event.target.id)
+		const deleteMe = persons.filter(p => p.id === id)[0]
+
+		if (window.confirm(`Do you really want to delete ${deleteMe.name}`)) {
+			contactService
+				.remove(id)
+				.then(setPersons(persons.filter(p => p.id !== id)))
+		}
+	}
+
+	return (
+		<div>
+			<h1>Phonebook</h1>
+			<Filter value={filter} onChange={handleFilterInput} />
+			<Form
+				nameValue={newName}
+				nameInput={handleNameInput}
+				numberValue={newNumber}
+				numberInput={handleNumberInput}
+				onSubmit={addPerson}
+			/>
+			<Persons peeps={persons} filter={filter} remove={removePerson} />
+		</div>
+	)
 }
 
 export default App
--- part2/phonebook/src/services/contacts.js
+++ part2/phonebook/src/services/contacts.js
@@ -4,12 +4,12 @@ const baseUrl = 'http://localhost:3001/persons'
 
 const getAll = () => axios.get(baseUrl).then(res => res.data)
 
-
-const add = newContact => axios
-    .post(baseUrl, newContact)
-    .then(res => res.data)
+const add = newContact => axios.post(baseUrl, newContact).then(res => res.data)
 
 const remove = id => axios.delete(`${baseUrl}/${id}`)
 
+const update = (id, updatedContact) => {
+	return axios.put(`${baseUrl}/${id}`, updatedContact).then(res => res.data)
+}
 
-export default {getAll, add, remove}
+export default { getAll, add, remove, update }
