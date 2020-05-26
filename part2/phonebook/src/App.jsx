87af68809630557c39d179ebae1db6a30c665a84
spezzers <54096233+spezzers@users.noreply.github.com>
2020-05-26 08:21:07 +0100

    2.19 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,12 +3,15 @@ import Form from './components/Form.jsx'
 import Filter from './components/Filter.jsx'
 import Persons from './components/Persons.jsx'
 import contactService from './services/contacts.js'
+import Notification from './components/Notification'
+import './index.css'
 
 const App = () => {
 	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
 	const [filter, setFilter] = useState('')
+	const [message, setMessage] = useState({type: 'success', content:'This is a notification'})
 
 	useEffect(() => {
 		contactService.getAll().then(initial => {
@@ -35,13 +38,15 @@ const App = () => {
 			if (window.confirm(`'${newName}' is already in the phonebook, update number?`)) {
 				const updatedContact = { ...chosenOne, number: newNumber }
 				contactService.update(chosenOne.id, updatedContact).then(res => {
-          setPersons(persons.map(p => (p.id !== chosenOne.id ? p : res)))
-          clearForm()
+					setPersons(persons.map(p => (p.id !== chosenOne.id ? p : res)))
+					showMessage('success', `Updated ${res.name}`)
+					clearForm()
 				})
 			}
 		} else {
 			contactService.add({ name: newName, number: newNumber }).then(res => {
 				setPersons(persons.concat(res))
+				showMessage('success', `Added ${res.name}`)
 				clearForm()
 			})
 		}
@@ -54,13 +59,23 @@ const App = () => {
 		if (window.confirm(`Do you really want to delete ${deleteMe.name}`)) {
 			contactService
 				.remove(id)
-				.then(setPersons(persons.filter(p => p.id !== id)))
+				.then(() => {
+					setPersons(persons.filter(p => p.id !== id))
+					showMessage('notify', `Deleted ${deleteMe.name}`)
+				})
 		}
+	}
+
+	const showMessage = (type, message, duration) => {
+		const showTime = duration || 4000
+		setMessage({type: type, content: message})
+		setTimeout(() => setMessage({type:'', content: ''}), showTime)
 	}
 
 	return (
 		<div>
 			<h1>Phonebook</h1>
+			<Notification type={message.type} content={message.content}/>
 			<Filter value={filter} onChange={handleFilterInput} />
 			<Form
 				nameValue={newName}
