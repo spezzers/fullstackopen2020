7d2546a0a374ae6a987cc55a070fe7a50de94be7
spezzers <54096233+spezzers@users.noreply.github.com>
2020-05-30 14:34:00 +0100

    baseUrl fix

--- part3/phonebook_frontend/src/services/contacts.js
+++ part3/phonebook_frontend/src/services/contacts.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://persons-book.herokuapp.com/api/persons'
 
 const getAll = () => axios.get(baseUrl).then(res => res.data)
 
