172f6802014dc01401c8fc7042146c129c8251ce
spezzers <54096233+spezzers@users.noreply.github.com>
2020-05-30 22:19:05 +0100

    proxy added

--- part3/phonebook_frontend/src/services/contacts.js
+++ part3/phonebook_frontend/src/services/contacts.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://persons-book.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => axios.get(baseUrl).then(res => res.data)
 
