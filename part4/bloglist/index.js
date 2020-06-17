056ca591e6169941027a4d391f7eb5bda0a79a0a
spezzers <54096233+spezzers@users.noreply.github.com>
2020-06-17 13:19:51 +0100

    4.2 done

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,29 +1,26 @@
-const logger = require('./utils/logger')
 const config = require('./utils/config')
-const http = require('http')
 const express = require('express')
 const app = express()
-app.use(express.json())
 const cors = require('cors')
-app.use(cors())
-const mongoose = require('mongoose')
 const blogListRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 
 logger.info('connecting to', config.mongoUrl)
 
+mongoose
+	.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		logger.info('connected to MongoDB')
+	})
+	.catch(error => {
+		logger.error('error connection to MongoDB:', error.message)
+	})
 
-
-mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {
-  logger.info('connected to MongoDB')
-})
-.catch((error) => {
-  logger.error('error connection to MongoDB:', error.message)
-})
-
+app.use(express.json())
+app.use(cors())
 app.use('/api/blogs', blogListRouter)
 
-
 app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}/`)
+	logger.info(`Server running on port ${config.PORT}/`)
 })
