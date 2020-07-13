7d9abf2c50f05be9fd8b605ad75855fed4a5fb17
spezzers <54096233+spezzers@users.noreply.github.com>
2020-07-13 18:19:38 +0100

    Error handling and async/await

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 ## Questions/further reading
 - Why do I need SuperTest, exactly?
     - Can I not test (HTTP) API requests without it or something?
+
+## async/await
+  - makes asynchronous code wait and behave like synchronous
+  - use `try-catch` to handle errors  
+    ```javascript
+    try{
+      ...
+    } catch(exception) {
+      next(exception)
+    }
+    ```
+    - the need for `try-catch` can be eliminated by using the [express-async-errors](https://github.com/davidbanham/express-async-errors): `npm install express-async-errors --save` 
--- part4/lessons/notes/app.js
+++ part4/lessons/notes/app.js
@@ -9,13 +9,17 @@ const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
+mongoose
+	.connect(config.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => {
+		logger.info('connected to MongoDB')
+	})
+	.catch(error => {
+		logger.error('error connection to MongoDB:', error.message)
+	})
 
 app.use(cors())
 app.use(express.static('build'))
