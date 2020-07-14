5b766c8f0c99e4ca25c1ee10c100a612703a2420
spezzers <54096233+spezzers@users.noreply.github.com>
2020-07-14 08:27:23 +0100

    exercises 4.8 - 4.12 start

--- README.md
+++ README.md
@@ -17,3 +17,19 @@
     }
     ```
     - the need for `try-catch` can be eliminated by using the [express-async-errors](https://github.com/davidbanham/express-async-errors): `npm install express-async-errors --save` 
+
+### Promises
+#### Promise.all
+The Promise.all method can be used for transforming an array of promises into a single promise, that will be fulfilled once every promise in the array passed to it as a parameter is resolved
+
+Promise.all executes the promises it receives in parallel. If the promises need to be executed in a particular order, this will be problematic. In situations like this, the operations can be executed inside of a for...of block, that guarantees a specific execution order.
+```javascript
+beforeEach(async () => {
+  await Note.deleteMany({})
+
+  for (let note of helper.initialNotes) {
+    let noteObject = new Note(note)
+    await noteObject.save()
+  }
+})
+```
--- part4/lessons/notes/test/note_api.test.js
+++ part4/lessons/notes/test/note_api.test.js
@@ -5,16 +5,13 @@ const api = supertest(app)
 const Note = require('../models/note')
 const helper = require('./test_helper')
 
-
-
 beforeEach(async () => {
 	await Note.deleteMany({})
-
-	let noteObject = new Note(helper.initialNotes[0])
-	await noteObject.save()
-
-	noteObject = new Note(helper.initialNotes[1])
-	await noteObject.save()
+	console.log('DB cleared')
+	const noteObjects = helper.initialNotes.map(note => new Note(note))
+	const promisArray = noteObjects.map(note => note.save())
+	await Promise.all(promisArray)
+	console.log('done')
 })
 
 test('notes are returned as json', async () => {
@@ -24,7 +21,7 @@ test('notes are returned as json', async () => {
 		.expect('Content-Type', /application\/json/)
 })
 
-test('all notes are returned', async () => {
+test.only('all notes are returned', async () => {
 	const response = await api.get('/api/notes')
 
 	expect(response.body).toHaveLength(helper.initialNotes.length)
@@ -61,17 +58,14 @@ test('note without content is not added', async () => {
 		important: true
 	}
 
-	await api
-		.post('/api/notes')
-		.send(newNote)
-		.expect(400)
+	await api.post('/api/notes').send(newNote).expect(400)
 
 	const notesAtEnd = await helper.notesInDb()
 
 	expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
 })
 
-test.only('a specific note can be viewed', async () => {
+test('a specific note can be viewed', async () => {
 	const notesAtStart = await helper.notesInDb()
 	const noteToView = notesAtStart[0]
 	const resultNote = await api
@@ -81,18 +75,15 @@ test.only('a specific note can be viewed', async () => {
 	expect(resultNote.body).toEqual(noteToView)
 })
 
-test.only('a note can be deleted', async () => {
+test('a note can be deleted', async () => {
 	const notesAtStart = await helper.notesInDb()
 	const noteToDelete = notesAtStart[0]
 
-	await api
-		.delete(`/api/notes/${noteToDelete.id}`)
+	await api.delete(`/api/notes/${noteToDelete.id}`)
 
 	const notesAtEnd = await helper.notesInDb()
 
-	expect(notesAtEnd).toHaveLength(
-		helper.initialNotes.length - 1
-	)
+	expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1)
 	const contents = notesAtEnd.map(r => r.content)
 	expect(contents).not.toContain(noteToDelete.content)
 })
