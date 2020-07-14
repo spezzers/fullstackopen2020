a1240a39d1b7b68721e96aba08ce5e5f6a80dfb8
spezzers <54096233+spezzers@users.noreply.github.com>
2020-07-14 10:50:17 +0100

    supertest

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,26 +1,10 @@
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogListRouter = require('./controllers/blogs')
+const app = require('./app')
+const http = require('http')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
 
-logger.info('connecting to', config.mongoUrl)
+const server = http.createServer(app)
 
-mongoose
-	.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => {
-		logger.info('connected to MongoDB')
-	})
-	.catch(error => {
-		logger.error('error connection to MongoDB:', error.message)
-	})
-
-app.use(express.json())
-app.use(cors())
-app.use('/api/blogs', blogListRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
 	logger.info(`Server running on port ${config.PORT}`)
 })
---
+++ part4/bloglist/tests/db.test.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../index')
+
+const api = supertest(app)
+
+test('gets all blogs', async () => {
+	await api
+		.get('/api/blogs')
+		.expect(200)
+		.expect('Content-Type', /application\/json/)
+})
+afterAll(() => {
+	mongoose.connection.close
+})
