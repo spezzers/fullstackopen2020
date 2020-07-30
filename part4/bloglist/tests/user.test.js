73cdf604ccd98a08a05d19b4fa1ec3f80d35fc2f
spezzers <54096233+spezzers@users.noreply.github.com>
2020-07-30 12:56:43 +0100

    4.15 done

--- part4/bloglist/tests/user.test.js
+++ part4/bloglist/tests/user.test.js
@@ -1,24 +1,53 @@
 const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose')
 const User = require('../models/User')
-const test_helper = require('./test_helper')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const api = supertest(app)
 
-describe('Initial test setup', () => {
-	test('1 + 2 = 3', async () => {
-		const simpleSum = 1 + 2
-		expect(simpleSum).toEqual(3)
+const saltRounds = 10
+
+beforeEach(async () => {
+	await User.deleteMany({})
+	const passwordHash = await bcrypt.hash('mysterious', saltRounds)
+	const user = new User({ username: 'root', name: 'Root User', passwordHash })
+	await user.save()
+})
+
+describe('Get All Users', () => {
+	beforeEach(async () => {
+		await User.deleteMany({})
+		const userObjects = helper.initialUsers.map(user => new User(user))
+		const promiseArray = userObjects.map(user => user.save())
+		await Promise.all(promiseArray)
+	})
+	test('response is JSON', async () => {
+		await api
+			.get('/api/users')
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
+	})
+	test('response has correct number of users', async () => {
+		const response = await api.get('/api/users')
+		expect(response.body.length).toBe(helper.initialUsers.length)
 	})
 })
 
 describe('4.15 - Create new user with HTTP Post', () => {
-	beforeEach(async () => {
-		await User.deleteMany({})
-		const passwordHash = await bcrypt.hash('mysterious', 10)
-		const user = new User({ username: 'root', passwordHash })
-
-		await user.save()
-	})
+	const newUser = helper.initialUsers[0]
 	test('successfully created a new user', async () => {
-		const usersAtStart = await test_helper.initialUsers
-		console.log(usersAtStart)
+		const response = await api.post('/api/users').send(newUser)
+		expect(response.body.name).toEqual(newUser.name)
+		expect(response.body.username).toEqual(newUser.username)
 	})
+	test('plain text password is not saved to DB', async () => {
+		const response = await api.post('/api/users').send(newUser)
+		expect(response.body.password).not.toBeDefined()
+	})
+})
+
+
+afterAll(() => {
+	mongoose.connection.close()
 })
