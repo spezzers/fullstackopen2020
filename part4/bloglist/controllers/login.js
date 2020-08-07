8e9108815a9e44ea5460acc432509866e258b773
Ben Morton-Sperry <54096233+spezzers@users.noreply.github.com>
2020-08-07 15:12:00 +0100

    4.18 done

--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -6,7 +6,6 @@ const User = require('../models/User')
 loginRouter.post('/', async (request, response) => {
 	const body = request.body
 	const user = await User.findOne({ username: body.username })
-	console.log(body.username, body.password)
 	const passwordCorrect = user === null
 		? false
 		: await bcrypt.compare(body.password, user.passwordHash)
@@ -22,7 +21,7 @@ loginRouter.post('/', async (request, response) => {
 	}
 
 	const token = jwt.sign(userForToken, process.env.SECRET)
-
+	console.log(`${body.username} has logged in`)
 	response
 		.status(200)
 		.send({token, username: user.username, name: user.name})
