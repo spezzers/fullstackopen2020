6f7837e4d6b4add71c2a1d516afa55ab95012d16
spezzers <54096233+spezzers@users.noreply.github.com>
2020-08-19 16:11:43 +0100

    login returns token

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,24 +1,69 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+	const [blogs, setBlogs] = useState([])
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  
+	useEffect(() => {
+		blogService.getAll().then(blogs => setBlogs(blogs))
   }, [])
+  
+  const handleLogin = async event => {
+    event.preventDefault()
+    console.log('logging in with ', username, password)
+    try {
+        const user = await loginService.login({
+            username, password
+        })
+        setUser(user)
+        setUsername('')
+        setPassword('')
+    } catch (exception) {
+        console.log('wrong credentials')
+    }
+  }
 
-  return (
+  const loginForm = () => (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+			<h2>Login</h2>
+			<form onSubmit={handleLogin}>
+				<div>
+					username
+					<input
+						type='text'
+						value={username}
+						name='Username'
+						onChange={({ target }) => setUsername(target.value)}
+					/>
+				</div>
+				<div>
+					password
+					<input
+						type='password'
+						value={password}
+						name='Password'
+						onChange={({ target }) => setPassword(target.value)}
+					/>
+				</div>
+				<button type='submit'>login</button>
+			</form>
+		</div>
   )
+
+	return (
+		<div>
+			{loginForm()}
+			<h2>blogs</h2>
+			{blogs.map(blog => (
+				<Blog key={blog.id} blog={blog} />
+			))}
+		</div>
+	)
 }
 
 export default App
