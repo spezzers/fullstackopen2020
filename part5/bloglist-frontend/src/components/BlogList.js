6cc5c5fc6a91ec070786232e9687c66a141fd117
spezzers <54096233+spezzers@users.noreply.github.com>
2020-08-21 11:40:35 +0100

    fix: useEffect stop infinite component activity

--- part5/bloglist-frontend/src/components/BlogList.js
+++ part5/bloglist-frontend/src/components/BlogList.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Blog from './Blog'
 import BlogForm from './BlogForm'
 import blogService from '../services/blogs'
 
 const BlogList = ({ user }) => {
 	const [blogs, setBlogs] = useState([])
+	useEffect (() => {
+		if (user !== null) {
+			blogService.getAll().then(blogs => setBlogs(blogs))
+		}
+	}, [user])
 	if (user === null) {
 		return null
 	}
-
-	blogService.getAll().then(blogs => setBlogs(blogs))
 	const blogList = blogs.map(blog => <Blog key={blog.id} blog={blog} />)
 
 	return (
