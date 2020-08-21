7fa451da1afcc3bcd761d8f39f1f0892ec38f290
spezzers <54096233+spezzers@users.noreply.github.com>
2020-08-21 09:17:17 +0100

    refactor Login

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,98 +1,40 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
-import loginService from './services/login'
+
+import Login from './components/Login'
 
 const App = () => {
 	const [blogs, setBlogs] = useState([])
-	const [username, setUsername] = useState('')
-	const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
-  
+	const [user, setUser] = useState(null)
+
 	useEffect(() => {
 		blogService.getAll().then(blogs => setBlogs(blogs))
-  }, [])
+	}, [])
 
-  useEffect(() => {
-    const loggedInUserJSON = window.localStorage.getItem('loggedInUser')
-    if(loggedInUserJSON) {
-      const user = JSON.parse(loggedInUserJSON)
-      setUser(user)
-    }
-  },[])
-  
-  const handleLogin = async event => {
-    event.preventDefault()
-    try {
-        const user = await loginService.login({
-            username, password
-        })
-        window.localStorage.setItem('loggedInUser', JSON.stringify(user))
-        setUser(user)
-        setUsername('')
-        setPassword('')
-    } catch (exception) {
-        alert('wrong credentials')
-    }
-  }
+	useEffect(() => {
+		const loggedInUserJSON = window.localStorage.getItem('loggedInUser')
+		if (loggedInUserJSON) {
+			const user = JSON.parse(loggedInUserJSON)
+			setUser(user)
+		}
+	}, [])
 
-  const loginForm = () => (
-    <div>
-			<h2>Login</h2>
-			<form onSubmit={handleLogin}>
-				<div>
-					username
-					<input
-						type='text'
-						value={username}
-						name='Username'
-						onChange={({ target }) => setUsername(target.value)}
-					/>
-				</div>
-				<div>
-					password
-					<input
-						type='password'
-						value={password}
-						name='Password'
-						onChange={({ target }) => setPassword(target.value)}
-					/>
-				</div>
-				<button type='submit'>login</button>
-			</form>
-		</div>
-  )
-
-  const handleLogout = (event) => {
-    event.preventDefault()
-    window.localStorage.removeItem('loggedInUser')
-    setUser(null)
-  }
-
-  const logout = () => {
-    return (
-      <div>
-        <button onClick={handleLogout}>logout</button>
-      </div>
-    )
-  }
-
-  const bloglist = () => {
-    const list = () =>  blogs.map(blog => (<Blog key={blog.id} blog={blog} />))
-    return (
-      <div>
-        Hello {user.name}
-        {logout()}
-        <h2>Blogs</h2>
-        {list()}
-      </div>
-    )
-  }
+	const bloglist = () => {
+		const list = () => blogs.map(blog => <Blog key={blog.id} blog={blog} />)
+		return (
+			<div>
+				Hello {user.name}
+				<Login user={user} setUser={setUser} />
+				<h2>Blogs</h2>
+				{list()}
+			</div>
+		)
+	}
 
 	return (
 		<div>
-			{user === null ? loginForm() : bloglist()}
-			
+			{user === null ? <Login user={user} setUser={setUser} /> : bloglist()}
 		</div>
 	)
 }
---
+++ part5/bloglist-frontend/src/components/Login.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import loginService from '../services/login'
+
+const Login = ({ user, setUser }) => {
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleLogin = async event => {
+		event.preventDefault()
+		try {
+			const user = await loginService.login({
+				username,
+				password
+			})
+			window.localStorage.setItem('loggedInUser', JSON.stringify(user))
+			setUser(user)
+			setUsername('')
+			setPassword('')
+		} catch (exception) {
+			alert('wrong credentials')
+		}
+	}
+
+	const handleLogout = event => {
+		event.preventDefault()
+		window.localStorage.removeItem('loggedInUser')
+		setUser(null)
+	}
+
+	if (user === null) {
+		return (
+			<div>
+				<h2>Login</h2>
+				<form onSubmit={handleLogin}>
+					<div>
+						username
+						<input
+							type='text'
+							value={username}
+							name='Username'
+							onChange={({ target }) => setUsername(target.value)}
+						/>
+					</div>
+					<div>
+						password
+						<input
+							type='password'
+							value={password}
+							name='Password'
+							onChange={({ target }) => setPassword(target.value)}
+						/>
+					</div>
+					<button type='submit'>login</button>
+				</form>
+			</div>
+		)
+	}
+	return (
+		<div>
+			<button onClick={handleLogout}>logout</button>
+		</div>
+	)
+}
+
+export default Login
