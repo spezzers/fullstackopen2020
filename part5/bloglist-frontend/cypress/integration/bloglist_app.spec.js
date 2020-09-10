3f04b71d43ea6c970452bffe75b32c034a1f772d
spezzers <54096233+spezzers@users.noreply.github.com>
2020-09-10 20:42:28 +0100

    5.19 done

--- part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
@@ -3,6 +3,12 @@ describe('Blog app', function() {
 		cy.request('POST', 'http://localhost:3003/api/testing/reset')
 		cy.visit('http://localhost:3000')
 	})
+	const user = {
+		id: 'ffffffffffffffffffffffff',
+		username: 'root',
+		name: 'Superuser',
+		password: 'supersecret'
+	}
 	describe('5.17', function() {
 		it('Display Login form by default', function() {
 			cy.contains('Login')
@@ -12,29 +18,45 @@ describe('Blog app', function() {
 	})
 	describe('5.18 - User Login', function() {
 		beforeEach(function() {
-			const user = {
-				username: 'root',
-				name: 'Superuser',
-				password: 'supersecret'
-			}
 			cy.request('POST', 'http://localhost:3003/api/users', user)
 		})
 		it('succeeds with correct credentials', function() {
 			cy.contains('Login')
-			cy.get('#username').type('root')
-			cy.get('#password').type('supersecret')
+			cy.get('#username').type(user.username)
+			cy.get('#password').type(user.password)
 			cy.get('#login-button').click()
 
 			cy.contains('Hello Superuser')
 		})
 		it('fails with wrong credentials', function() {
 			cy.contains('Login')
-			cy.get('#username').type('root')
-			cy.get('#password').type('semisecret')
+			cy.get('#username').type(user.username)
+			cy.get('#password').type('wrongsecret')
 			cy.get('#login-button').click()
 
 			cy.contains('wrong username or password')
 
+		})
+	})
+	describe.only('5.19 - When logged in', function() {
+		beforeEach(function() {
+			cy.request('POST', 'http://localhost:3003/api/users', user)
+			cy.request('POST', 'http://localhost:3003/api/login', {
+				username: 'root', password: 'supersecret'
+			}).then(response => {
+				localStorage.setItem('loggedInUser', JSON.stringify(response.body))
+				cy.visit('http://localhost:3000')
+			})
+		})
+		it('A blog can be created', function() {
+			cy.contains('Blogs')
+			cy.contains('Add a blog').click()
+			cy.get('#form #title').type('A Brand New Blog')
+			cy.get('#form #author').type('New Kid')
+			cy.get('#form #url').type('http://blog.newkid.com/a-brand-new-blog')
+			cy.contains('Add blog').click()
+			cy.contains('Blog Added')
+			cy.contains('A Brand New Blog - New Kid')
 		})
 	})
 })
