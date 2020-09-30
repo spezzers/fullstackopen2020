e33847bdcc7bf9658815184bf57b1b960f18f894
spezzers <54096233+spezzers@users.noreply.github.com>
2020-09-30 18:46:38 +0100

    refactoring components

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -1,50 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
+import { useCountry, useField } from './hooks'
+import Country from './components/Country'
 
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
-const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
-
-  useEffect()
-
-  return country
-}
-
-const Country = ({ country }) => {
-  if (!country) {
-    return null
-  }
-
-  if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
-    </div>
-  )
-}
 
 const App = () => {
   const nameInput = useField('text')
