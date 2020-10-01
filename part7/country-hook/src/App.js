a73f729a39048ebb9d3a9f779b1d1d534b76f2a4
spezzers <54096233+spezzers@users.noreply.github.com>
2020-10-01 07:57:43 +0100

    7.7 done

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -1,29 +1,27 @@
 import React, { useState } from 'react'
-import axios from 'axios'
 import { useCountry, useField } from './hooks'
 import Country from './components/Country'
 
-
 const App = () => {
-  const nameInput = useField('text')
-  const [name, setName] = useState('')
-  const country = useCountry(name)
+	const nameInput = useField('text')
+	const [name, setName] = useState('')
+	const country = useCountry(name)
 
-  const fetch = (e) => {
-    e.preventDefault()
-    setName(nameInput.value)
-  }
+	const fetch = e => {
+		e.preventDefault()
+		setName(nameInput.value)
+	}
 
-  return (
-    <div>
-      <form onSubmit={fetch}>
-        <input {...nameInput} />
-        <button>find</button>
-      </form>
+	return (
+		<div>
+			<form onSubmit={fetch}>
+				<input {...nameInput} />
+				<button>find</button>
+			</form>
 
-      <Country country={country} />
-    </div>
-  )
+			<Country country={country} />
+		</div>
+	)
 }
 
 export default App
--- part7/country-hook/src/hooks/index.js
+++ part7/country-hook/src/hooks/index.js
@@ -1,10 +1,21 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export const useCountry = name => {
 	const [country, setCountry] = useState(null)
 
-	// useEffect()
-
+	useEffect(() => {
+		if (name) {
+			axios
+				.get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+				.then(res => {
+					const data = res.data[0]
+					setCountry({data, found: true})
+				})
+				.catch(setCountry({found: false}))
+		}
+	}, [name])
+	
 	return country
 }
 
