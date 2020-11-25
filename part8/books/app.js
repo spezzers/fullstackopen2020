d81554a3b27d3b29e68b8e980d22657649d080b0
spezzers <54096233+spezzers@users.noreply.github.com>
2020-11-25 14:53:33 +0000

    refactoring and User model

---
+++ part8/books/app.js
@@ -0,0 +1,135 @@
+const { v1: uuid } = require('uuid')
+const { gql, UserInputError } = require('apollo-server')
+const Author = require('./models/Author')
+const Book = require('./models/Book')
+
+const typeDefs = gql`
+	type User {
+		username: String!
+		favouriteGenre: String!
+		id: ID!
+	}
+	type Token {
+		value: String!
+	}
+	type Author {
+		name: String!
+		born: String
+		id: ID!
+		bookCount: Int
+	}
+	type Book {
+		title: String!
+		published: Int!
+		author: Author!
+		genres: [String!]!
+		id: ID!
+	}
+	type Query {
+		bookCount(author: String): Int!
+		authorCount: Int!
+		allBooks(author: String, genre: String): [Book!]!
+		allAuthors: [Author!]!
+		me: User
+	}
+	type Mutation {
+		addBook(
+			title: String!
+			author: String!
+			genres: [String]
+			published: Int!
+		): Book
+		editAuthor(name: String!, setBornTo: Int!): Author
+		createUser(username: String!, favouriteGenre: String!): User
+		login(username: String!, password: String!): Token
+	}
+`
+
+const resolvers = {
+	Query: {
+		bookCount: () => Book.collection.countDocuments(),
+		authorCount: () => Author.collection.countDocuments(),
+		allBooks: async (root, args) => {
+			let bookList = await Book.find({}).populate('author')
+			return bookList
+		},
+		allAuthors: async () => await Author.find({})
+	},
+	Author: {
+		bookCount: async root => {
+			const result = await Book.find({ author: root.id })
+			return result.length
+		}
+	},
+
+	Mutation: {
+		addBook: async (root, args) => {
+			console.log(
+				JSON.stringify(
+					{
+						Mutation: {
+							addBook: args
+						}
+					},
+					null,
+					2
+				)
+			)
+			const books = await Book.find({})
+			if (books.find(b => b.title === args.title)) {
+				throw new UserInputError('This title already exists', {
+					invalidArgs: args.title
+				})
+			}
+			const existingAuthor = await Author.findOne({ name: args.author })
+
+			const newAuthor = new Author({
+				name: args.author,
+				id: uuid()
+			})
+			let newBookAuthor
+
+			existingAuthor
+				? (newBookAuthor = existingAuthor)
+				: (newBookAuthor = await newAuthor.save())
+
+			const newBook = new Book({
+				...args,
+				id: uuid(),
+				author: newBookAuthor
+			})
+
+			try {
+				const result = await newBook.save()
+				return result.data
+			} catch (error) {
+				throw new UserInputError(error.message)
+			}
+		},
+		editAuthor: async (root, args) => {
+			const author = await Author.findOne({ name: args.name })
+			if (!author) {
+				return null
+			}
+			if (args.setBornTo.toString().length < 4) {
+				throw new UserInputError('birth year should be 4 digits long')
+			}
+			const updatedAuthor = {
+				...author._doc,
+				born: args.setBornTo
+			}
+			try {
+				return await Author.findByIdAndUpdate(author.id, updatedAuthor, {
+					new: true
+				})
+			} catch (error) {
+				throw new UserInputError(error.message)
+			}
+		}
+	}
+}
+
+module.exports = {
+	typeDefs,
+	resolvers
+}
---
+++ part8/books/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+		minLength: 3
+	},
+	favouriteGenre: {
+		type: String,
+		required: true
+	},
+	passwordHash: {
+		type: String,
+		required: true
+	}
+})
+
+userSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id._.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+		delete returnedObject.passwordHash
+	}
+})
+
+userSchema.plugin(uniqueValidator)
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
