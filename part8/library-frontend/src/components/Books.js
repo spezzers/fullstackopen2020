a2e654e0e26bced5b2c79bb5b91adff9e17f4b5c
spezzers <54096233+spezzers@users.noreply.github.com>
2020-12-01 15:24:33 +0000

    - refactoring - custom hook useBookList created

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,70 +1,21 @@
-import React, { useState } from 'react'
-import { useQuery } from '@apollo/client'
-import { ALL_BOOKS } from '../queries'
+import React from 'react'
+import { useBookList } from '../hooks'
 
 const Books = props => {
-	const [filter, setFilter] = useState('')
 
-	const books = useQuery(ALL_BOOKS)
-
-	const allBooks = books.data && books.data.allBooks ? books.data.allBooks : []
-
-	const genres = allBooks
-		? allBooks
-				.flatMap(book => book.genres)
-				.reduce((acc, cur) => {
-					if (acc.includes(cur)) {
-						return [...acc]
-					}
-					return acc.concat(cur)
-				}, [])
-		: []
-
-	const booksToShow =
-		filter === ''
-			? allBooks
-			: allBooks.filter(book => book.genres.includes(filter))
-
-	// ------------------------ conditional rendering
-
+	const books = useBookList('Books')
+	
 	if (!props.show) {
 		return null
 	}
 
-	if (books.loading) {
-		return <div>Loading...</div>
-	}
-
-	// -------------------------------------------
-
 	return (
 		<div>
-			<h2>books</h2>
-			{filter !== '' ? (
-				<p>
-					in genre: <strong>{filter}</strong>
-				</p>
-			) : null}
-			<table>
-				<tbody>
-					<tr>
-						<th>Book Title</th>
-						<th>Author</th>
-						<th align='center'>Published</th>
-					</tr>
-					{booksToShow.map(a => (
-						<tr key={a.title}>
-							<td>{a.title}</td>
-							<td>{a.author.name}</td>
-							<td align='center'>{a.published}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
+			{books.component}
 			<div>
-				<button onClick={() => setFilter('')}>all genres</button>
-				{genres.map(g => (
-					<button key={g} onClick={() => setFilter(g)}>
+				<button onClick={() => books.setFilter('')}>all genres</button>
+				{books.genres.map(g => (
+					<button key={g} onClick={() => books.setFilter(g)}>
 						{g}
 					</button>
 				))}
--- part8/library-frontend/src/hooks.js
+++ part8/library-frontend/src/hooks.js
@@ -1,4 +1,8 @@
 import React, {useState} from 'react'
+import { useQuery } from '@apollo/client'
+import { ALL_BOOKS } from './queries'
+
+
 
 export const useMessage = () => {
 	const [message, setMessage] = useState('')
@@ -21,4 +25,72 @@ export const useMessage = () => {
 		display,
 		newMessage
 	}
+}
+
+export const useBookList = (title, initFilter) => {
+	const [filter, setFilter] = useState(initFilter || '')
+
+	const books = useQuery(ALL_BOOKS)
+
+	const allBooks = books.data && books.data.allBooks ? books.data.allBooks : []
+
+	const genres = allBooks
+		? allBooks
+				.flatMap(book => book.genres)
+				.reduce((acc, cur) => {
+					if (acc.includes(cur)) {
+						return [...acc]
+					}
+					return acc.concat(cur)
+				}, [])
+		: []
+
+	const booksToShow =
+		filter === ''
+			? allBooks
+			: allBooks.filter(book => book.genres.includes(filter))
+
+	// ------------------------ conditional rendering
+
+	// if (!props.show) {
+	// 	return null
+	// }
+
+
+	// -------------------------------------------
+
+	const component = books.loading
+		? <div>Loading...</div>
+		: (
+		<div>
+			<h2>{title}</h2>
+			{filter !== '' ? (
+				<p>
+					in genre: <strong>{filter}</strong>
+				</p>
+			) : null}
+			<table>
+				<tbody>
+					<tr>
+						<th>Book Title</th>
+						<th>Author</th>
+						<th align='center'>Published</th>
+					</tr>
+					{booksToShow.map(a => (
+						<tr key={a.title}>
+							<td>{a.title}</td>
+							<td>{a.author.name}</td>
+							<td align='center'>{a.published}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	)
+	return {
+		component,
+		setFilter,
+		genres
+	}
+
 }
