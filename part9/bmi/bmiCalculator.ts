9b9d3e427d08f5f43683850e79ba660e6b8dc704
spezzers <54096233+spezzers@users.noreply.github.com>
2021-01-18 18:33:37 +0000

    CLI for bmiCalculator

--- part9/bmi/bmiCalculator.ts
+++ part9/bmi/bmiCalculator.ts
@@ -26,8 +26,25 @@ const calculateBMI = (height: cm, mass: kg): string => {
     }
 };
 
+const parseArgs = (args: string[]): { height: cm; mass: kg } => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            mass: Number(args[3]),
+        };
+    } else {
+        throw new Error('Provided values were not numbers');
+    }
+};
+
+const height: cm = Number(process.argv[2]);
+
 try {
-    console.log(calculateBMI(180, 74));
+    const { height, mass } = parseArgs(process.argv);
+    console.log(calculateBMI(height, mass));
 } catch (e) {
     console.log('Something went wrong, error message: ', e.message);
 }
